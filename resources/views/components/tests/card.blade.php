5f3e557b140929c74a4124e9c4b6f5f95ce5a815
taka1214 <globar173@gmail.com>
2023-01-18 16:24:56 +0900

    属性バッグ

--- resources/views/components/tests/card.blade.php
+++ resources/views/components/tests/card.blade.php
@@ -1,4 +1,12 @@
-<div class="border-2 shadow-md w-1/4 p-2">
+@props([
+  'title' => 'タイトル初期値',
+  'message' => '初期値',
+  'content' => '本文初期値' 
+])
+
+<div {{ $attributes->merge([
+    'class' => 'border-2 shadow-md w-1/4 p-2'
+  ]) }}>
   <div>{{ $title }}</div>
   <div>画像</div>
   <div>{{ $content }}</div>
--- resources/views/tests/component-test1.blade.php
+++ resources/views/tests/component-test1.blade.php
@@ -2,4 +2,6 @@
   <x-slot name="header">ヘッダー</x-slot>
   コンポーネントテスト１
   <x-tests.card title="タイトル" content="本文" :message="$message"></x-tests.card>
+  <x-tests.card title="タイトル２"></x-tests.card>
+  <x-tests.card title="背景色変更" class="bg-red-400"></x-tests.card>
 </x-tests.app>
